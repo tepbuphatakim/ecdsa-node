d21574c1decf972442621eb848c211376f300746
Tepbuphata Kim <kimtepbuphata009@gmail.com>
2024-07-25 17:24:17 +0700

    wip: digital signature with ecdsa practice

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -7,17 +7,22 @@ import { useState } from 'react';
 function App() {
   const [balance, setBalance] = useState(0);
   const [address, setAddress] = useState('');
+  const [account, setAccount] = useState({
+    address: '',
+    publicKey: '',
+    privateKey: '',
+  });
 
   return (
     <div className="app">
-      <Account />
+      <Account account={account} setAccount={setAccount} />
       <Wallet
         balance={balance}
         setBalance={setBalance}
         address={address}
         setAddress={setAddress}
       />
-      <Transfer setBalance={setBalance} address={address} />
+      <Transfer setBalance={setBalance} address={address} account={account} />
     </div>
   );
 }
